b2e7c4b2927c1101b1631b17ed440b292f9b4be7
Jelena <jelena33.bliss@gmail.com>
2026-05-23 13:26:57 +0200

    Initial admin setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-amber-300'>Hello cats</h1>
+    </div>
+  )
+}
+
+export default App
+
