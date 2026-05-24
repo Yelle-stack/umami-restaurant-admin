316a4634e23fe673aacf1ebf85b855bcfa8488c3
Jelena <jelena33.bliss@gmail.com>
2026-05-24 12:34:18 +0200

    Update admin login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,17 +5,17 @@ const Login = () => {
     <div>
       <div className='flex justify-center items-center min-h-screen bg-gray-100'>
         <div className='bg-white shadow-md rounded-lg px-8 py-6 w-full max-w-md'>
-          <h1 className='text-2xl'>Admin Login</h1>
+          <h1 className='text-2xl font-bold text-center text-gray-800 mb-4'>Admin Login</h1>
           <form>
-            <div>
-              <p>Email Address</p>
-               <input type="email" placeholder='Enter email' required/>
+            <div className='mb-4'>
+              <p className='text-sm font-semibold text-gray-600 mb-2'>Email Address</p>
+               <input type="email" placeholder='Enter email' required className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
             </div>
-            <div>
-              <p>Password</p>
-               <input type="password" placeholder='Enter Password' required/>
+            <div className='mb-4'>
+              <p className='text-sm font-semibold text-gray-600 mb-2'>Password</p>
+               <input type="password" placeholder='Enter Password' required className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
             </div>
-            <button type='submit'>Login</button>
+            <button type='submit' className='w-full px-3 py-2 text-lg font-bold bg-amber-500 rounded-md'>Login</button>
           </form>
         </div>
       </div>
