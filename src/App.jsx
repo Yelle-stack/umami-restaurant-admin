c045a2e7dd3eb86a6cf1939c5035d8de01d1ea53
Jelena <jelena33.bliss@gmail.com>
2026-05-25 19:00:49 +0200

    Create sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Login from './components/Login'
+import Sidebar from './components/Sidebar'
 
 
 export const backendUrl = 'http://localhost:4000'
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div>
       <Login/>
+      <Sidebar/>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { IoIosLogOut, IoMdAddCircleOutline } from 'react-icons/io'
+import { MdFormatListBulletedAdd } from 'react-icons/md'
+import { PiListBulletsFill } from 'react-icons/pi'
 
 const Sidebar = () => {
   return (
     <div>
-      
+      <div>
+        <h2>Umami House</h2>
+      </div>
+
+      <div>
+
+        <NavLink to="/add-menu">
+          <IoMdAddCircleOutline />
+          <p>Add Menu</p>
+        </NavLink>
+
+        <NavLink to="/menu-list">
+          <MdFormatListBulletedAdd />
+          <p>Menu List</p>
+        </NavLink>
+
+        <NavLink to="/reservations">
+          <PiListBulletsFill />
+          <p>Reservations</p>
+        </NavLink>
+
+        <button>
+          <IoIosLogOut />
+          <p>Logout</p>
+        </button>
+
+      </div>
     </div>
   )
 }
