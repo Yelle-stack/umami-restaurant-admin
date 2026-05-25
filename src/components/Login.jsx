6b15fb8f1a5ee8e67d4d0fb7fca6486a194f5fed
Jelena <jelena33.bliss@gmail.com>
2026-05-25 11:08:43 +0200

    Update admin login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div>
       <div className='flex justify-center items-center min-h-screen bg-gray-100'>
@@ -9,11 +13,21 @@ const Login = () => {
           <form>
             <div className='mb-4'>
               <p className='text-sm font-semibold text-gray-600 mb-2'>Email Address</p>
-               <input type="email" placeholder='Enter email' required className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
+               <input 
+               type="email" 
+               value={email} 
+               onChange={(e) => setEmail(e.target.value)}
+               placeholder='Enter email' required 
+               className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
             </div>
             <div className='mb-4'>
               <p className='text-sm font-semibold text-gray-600 mb-2'>Password</p>
-               <input type="password" placeholder='Enter Password' required className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
+               <input 
+               type="password" 
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               placeholder='Enter Password' required 
+               className='w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-800'/>
             </div>
             <button type='submit' className='w-full px-3 py-2 text-lg font-bold bg-amber-500 rounded-md'>Login</button>
           </form>
