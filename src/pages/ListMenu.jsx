400403d8683dad722c995ae183b496cba3496212
Jelena <jelena33.bliss@gmail.com>
2026-06-02 18:15:17 +0200

    listMenu

--- src/pages/ListMenu.jsx
+++ src/pages/ListMenu.jsx
@@ -27,23 +27,28 @@ const ListMenu = ({token}) => {
   },[])
   return (
     <div>
-      <p>Menu List</p>
-      <div>
-        <div>
+      <p className='mb-2 font-bold text-2xl'>Menu List</p>
+      <div className='flex flex-col gap-2'>
+        <div className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center p-2 border-b-2 border-gray-300 text-lg font-semibold'>
           <b>Image</b>
           <b>Name</b>
           <b>Category</b>
           <b>Price</b>
-          <b>Action</b>
+          <b className='text-center'>Action</b>
         </div>
          
          {list.map((item, index) => 
-        <div key={index}>
-          <img src={item.image} alt="" />
+        <div 
+        key={index} 
+        className='grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center p-2 border-b-2 border-gray-300 text-lg'>
+          <img 
+          className='w-12.5 h-auto'
+          src={item.image} 
+          alt="" />
           <p>{item.name}</p>
           <p>{item.category}</p>
           <p>{item.price} €</p>
-          <MdDeleteForever/>
+          <MdDeleteForever className='ml-10 text-[28px] cursor-pointer text-red-700'/>
 
         </div>)}
 
