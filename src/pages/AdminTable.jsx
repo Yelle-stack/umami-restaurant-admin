d773563cabfadc8de4be2e76b055dfe7f34bec1d
Jelena <jelena33.bliss@gmail.com>
2026-06-04 14:20:32 +0200

    adminTable

--- src/pages/AdminTable.jsx
+++ src/pages/AdminTable.jsx
@@ -24,8 +24,48 @@ const AdminTable = () => {
 fetchReservations()
   },[])
   return (
-    <div>
-      AdminTable
+    <div className='min-h-screen'>
+      <h2>Restaurant Reservations</h2>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Guests</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              reservations.length === 0 ? (
+                <tr>
+                  <td colSpan="7">No reservations found</td>
+                </tr>
+              ) : (
+                reservations.map((res, index)=> (
+                  <tr key={index}>
+                    <td>{res.name}</td>
+                    <td>{res.email}</td>
+                    <td>{res.phone}</td>
+                    <td>{res.date}</td>
+                    <td>{res.time}</td>
+                    <td>{res.guests}</td>
+                    <td>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
